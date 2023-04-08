6e31720c0ed53fa57d05e6c80ae01d708586dd19
JR DevPM <astronomi@gmail.com>
2023-04-08 17:12:55 -0300

    Setting initial FaceDetector

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react"
 import { StyleSheet, View } from "react-native"
 import { Camera, CameraType } from "expo-camera"
+import * as FaceDetector from "expo-face-detector"
 
 const styles = StyleSheet.create({
   container: {
@@ -24,7 +25,17 @@ export function Home() {
 
   return (
     <View style={styles.container}>
-      <Camera type={CameraType.front} style={styles.camera} />
+      <Camera
+        type={CameraType.front}
+        style={styles.camera}
+        faceDetectorSettings={{
+          mode: FaceDetector.FaceDetectorMode.fast,
+          detectLandmarks: FaceDetector.FaceDetectorLandmarks.none,
+          runClassifications: FaceDetector.FaceDetectorClassifications.none,
+          minDetectionInterval: 100,
+          tracking: true,
+        }}
+      />
     </View>
   )
 }

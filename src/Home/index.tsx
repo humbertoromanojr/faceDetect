96817a4a05a92293e2033648d4af23be500fe46c
JR DevPM <astronomi@gmail.com>
2023-04-08 17:45:57 -0300

    Setting FaceDetector with react-native-reanimated

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,7 +1,11 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { StyleSheet, View } from "react-native"
-import { Camera, CameraType } from "expo-camera"
+import { Camera, CameraType, FaceDetectionResult } from "expo-camera"
 import * as FaceDetector from "expo-face-detector"
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+} from "react-native-reanimated"
 
 const styles = StyleSheet.create({
   container: {
@@ -13,7 +17,48 @@ const styles = StyleSheet.create({
 })
 
 export function Home() {
+  const [faceDetected, setFaceDetected] = useState(false)
   const [permission, requestPermission] = Camera.useCameraPermissions()
+
+  const faceValues = useSharedValue({
+    width: 0,
+    height: 0,
+    x: 0,
+    y: 0,
+  })
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    position: "absolute",
+    zIndex: 1,
+    width: faceValues.value.width,
+    height: faceValues.value.height,
+    transform: [
+      { translateX: faceValues.value.x },
+      { translateY: faceValues.value.y },
+    ],
+    borderColor: "blue",
+    borderWidth: 10,
+  }))
+
+  function handleFaceDetected({ faces }: FaceDetectionResult) {
+    //console.log("=== faces ===", faces)
+    const face = faces[0] as any
+
+    if (face) {
+      const { size, origin } = face.bounds
+
+      faceValues.value = {
+        width: size.width,
+        height: size.height,
+        x: origin.x,
+        y: origin.y,
+      }
+
+      setFaceDetected(true)
+    } else {
+      setFaceDetected(false)
+    }
+  }
 
   useEffect(() => {
     requestPermission()
@@ -25,13 +70,15 @@ export function Home() {
 
   return (
     <View style={styles.container}>
+      {faceDetected && <Animated.View style={animatedStyle} />}
       <Camera
         type={CameraType.front}
         style={styles.camera}
+        onFacesDetected={handleFaceDetected}
         faceDetectorSettings={{
           mode: FaceDetector.FaceDetectorMode.fast,
-          detectLandmarks: FaceDetector.FaceDetectorLandmarks.none,
-          runClassifications: FaceDetector.FaceDetectorClassifications.none,
+          detectLandmarks: FaceDetector.FaceDetectorLandmarks.all,
+          runClassifications: FaceDetector.FaceDetectorClassifications.all,
           minDetectionInterval: 100,
           tracking: true,
         }}
